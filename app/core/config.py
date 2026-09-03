d12a2f7d571fb620398f9f4da00b2e3ac6f9f9cf
17-sabbir <sabbir05m.nstu@gmail.com>
2026-09-03 17:05:35 +0600

    increase notification re-notify interval from 2 to 4 minutes

--- app/core/config.py
+++ app/core/config.py
@@ -25,7 +25,7 @@ class Settings(BaseSettings):
     RATE_LIMIT_AUTH: str = "5/minute"
     RATE_LIMIT_OTP: str = "5/15minutes"
     RATE_LIMIT_RESEND: str = "1/minute"
-    NOTIFICATION_RENOTIFY_INTERVAL_MINUTES: PositiveInt = 2
+    NOTIFICATION_RENOTIFY_INTERVAL_MINUTES: PositiveInt = 4
     NOTIFICATION_SCHEDULER_INTERVAL_SECONDS: PositiveInt = 10
     FCM_SEND_CONCURRENCY: PositiveInt = 20
 
